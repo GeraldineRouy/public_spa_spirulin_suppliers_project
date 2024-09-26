8ed79816cb0b26e2854f5ac552cde94dc6ed8064
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-26 16:44:51 +0200

    Etape 6 : wip

--- spirulin-suppliers-finder/src/components/SuppliersList.vue
+++ spirulin-suppliers-finder/src/components/SuppliersList.vue
@@ -5,9 +5,20 @@ export default {
   components: {Supplier},
   data () {
     return {
-      name: 'John Doe',
-      status: false,
-      checkedAt: new Date(),
+      suppliers: [
+        {
+          id: 1,
+          name: "Fournisseur 1 ",
+          status: true,
+          checkedAt: new Date()
+        },
+        {
+          id: 2,
+          name: "Fournisseur 2",
+          status: false,
+          checkedAt: new Date()
+        }
+      ]
     }
   }
 }
@@ -16,15 +27,32 @@ export default {
 <template>
   <h2>Liste des fournisseurs</h2>
   <p>Ceci est la liste des fournisseurs.</p>
-  <div>
-    <h2>{{name}}</h2>
-    <p v-if="status">Stock OK :-)</p>
-    <p v-else>Stock KO :-(</p>
+
+  <div v-for="supplier in suppliers">
+
+    <h2>{{supplier.name}}</h2>
+
+    <span :class="{'isOK': supplier.status, 'isNotOk': !supplier.status}">
+      <strong v-if="supplier.status">Stock OK :-)</strong>
+      <strong v-else>Stock KO :-(</strong>
+    </span>
+
     <p>Dernière mise à jour du stock : {{ formatedCheckedAt }}</p>
+
   </div>
 
 </template>
 
 <style scoped>
+
+.isOK{
+  color: green;
+  font-weight: bold;
+}
+
+.isNotOk{
+  color: red;
+  font-weight: bold;
+}
 
 </style>
