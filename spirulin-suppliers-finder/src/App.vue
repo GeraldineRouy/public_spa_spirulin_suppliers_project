f1be2100771ff1ab21818c364109b3835ba4c31c
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-27 13:42:09 +0200

    Etape 8.1

---
+++ spirulin-suppliers-finder/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Home from "@/components/Home.vue";
+
+</script>
+
+<template>
+
+  <main>
+    <Home />
+        <nav>
+          <router-link :to= '{ name : "SuppliersList" }'>Afficher la liste des fournisseurs</router-link>
+          <router-link to='/map'>Afficher la carte</router-link>
+<!--          <router-link :to="{ name : 'Supplier' }">Afficher un fournisseur</router-link>-->
+        </nav>
+    <router-view></router-view>
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
