1050d01d2aed5ea6e1a784d2c121064b092c23d5
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-27 11:54:14 +0200

    Etape 7.2: Affichage de marqueurs dans Map.vue OK

--- spirulin-suppliers-finder/src/components/Map.vue
+++ spirulin-suppliers-finder/src/components/Map.vue
@@ -1,16 +1,3 @@
-<template>
-  <div class="map">
-    <LMap :zoom="zoom" :center="center">
-      <LTileLayer :url="url"></LTileLayer>
-<!--      <LMarker :lat-lng="[47.413220, -1.219482]"></LMarker>-->
-<!--      <LMarker :lat-lng="[46.193220, 4.82]"></LMarker>-->
-<!--      <LMarker :lat-lng="[45.193220, 6.82]"></LMarker>-->
-<!--      <LMarker :lat-lng="[47.03220, -0.9482]"></LMarker>-->
-<!--      <LMarker :lat-lng="[46.03220, 2.9482]"></LMarker>-->
-    </LMap>
-  </div>
-</template>
-
 <script>
 import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 
@@ -26,11 +13,32 @@ export default {
       url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
       zoom: 6,
       center: [46.5322, 2.9482],
-      bounds: null
-    };
+      bounds: null,
+      suppliers: [
+        {
+          id: 1,
+          latitude: 10,
+          longitude: 10
+        },
+        {
+          id: 2,
+          latitude: 11,
+          longitude: 9.6
+        }
+      ]
+    }
   }
 };
 </script>
+
+<template>
+  <div class="map">
+    <LMap :zoom="zoom" :center="center">
+      <LTileLayer :url="url"></LTileLayer>
+      <l-marker v-for="supplier in suppliers" :lat-lng="[supplier.latitude, supplier.longitude]"></l-marker>
+    </LMap>
+  </div>
+</template>
 
 <style scoped>
   .map{
