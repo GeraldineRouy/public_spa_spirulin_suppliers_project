e909759c0b1b98c3efc16c9d2fd43be162bc1a79
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-27 11:24:04 +0200

    Etape 7.1: Ajout du composant Map, affichage de la carte vide

--- spirulin-suppliers-finder/src/components/SuppliersMap.vue
+++ spirulin-suppliers-finder/src/components/SuppliersMap.vue
@@ -1,14 +1,25 @@
 <script>
+import Map from "@/components/Map.vue";
+// import "leaflet/dist/leaflet.css";
+// import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
 export default {
-  name: "SuppliersMap"
+  name: "SuppliersMap",
+  components: {
+    Map
+  }
 }
 </script>
 
 <template>
 <h2>Carte des fournisseurs</h2>
   <p>Ici, la carte des fournisseurs.</p>
+
+  <Map/>
+
 </template>
 
 <style scoped>
+
 
 </style>
